e3ff318c6f8cbf0a6fda6bb6031b87c21e536d8b
lendle <66368837+Lendle-King@users.noreply.github.com>
2026-09-17 17:40:19 +0800

    style: normalize dsh executor test imports

--- tests/test_dsh_headless_executor.py
+++ tests/test_dsh_headless_executor.py
@@ -5,10 +5,10 @@ import sys
 import textwrap
 
 import pytest
-from epipilot.executors.dsh_headless import DshHeadlessExecutor
 
 from epipilot.core.models import Task, new_task_id
 from epipilot.executors.base import ExecutorState
+from epipilot.executors.dsh_headless import DshHeadlessExecutor
 
 _FINAL_SUCCESS_SCRIPT = textwrap.dedent(
     """
